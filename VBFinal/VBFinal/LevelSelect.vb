166324c54c10305e162f30a34fc7ee089280c2f0
Andrew <heldiggris7@gmail.com>
2018-01-03 10:27:44 -0600

    update LevelSelcet Des

--- VBFinal/VBFinal/LevelSelect.vb
+++ VBFinal/VBFinal/LevelSelect.vb
@@ -1,3 +1,7 @@
 ﻿Public Class LevelSelect
 
+    Private Sub btnMainMenu_Click(sender As Object, e As EventArgs) Handles btnMainMenu.Click
+        Me.Close()
+        MainMenu.Visible = True
+    End Sub
 End Class
